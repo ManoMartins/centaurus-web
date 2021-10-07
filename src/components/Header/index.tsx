e0e8c3d9313b16e7a6c173955bff4326b145f1bd
Manoel <mano.martins29@gmail.com>
2021-10-07 06:06:41 -0300

    fix: logo name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <Container>
       <div>
-        <Logo>Centeurus</Logo>
+        <Logo>Centaurus</Logo>
         <Navigation />
       </div>
     </Container>
