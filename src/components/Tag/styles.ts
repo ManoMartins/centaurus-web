f38e2dceadc584c5120477ce9f2c0c76ed8acfbe
Manoel <mano.martins29@gmail.com>
2021-10-07 06:33:25 -0300

    style: adjust when tags overflow element

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -9,6 +9,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center; 
+  margin: 2px;
 
   a {
     display: flex;
--- src/components/Tags/styles.ts
+++ src/components/Tags/styles.ts
@@ -2,19 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 export const Title = styled.span`
   font-size: 16px;
   color: #E5E5E5;
   width: 100px;
+
 `;
 
 export const TagList = styled.div`
   display: flex;
-
-  > div + div {
-    margin-left: 4px;
-  }
+  flex-wrap: wrap;
+  width: 1100px;
 `;
